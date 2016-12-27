231cdccb4b1414f33915ddc631feb9d09cc425cb
Jason S Zang <jasonszang@126.com>
2016-12-27 19:23:18 +0800

    A fair spin lock based on the ticket lock algorithm

--- concurrency/spin_lock.h
+++ concurrency/spin_lock.h
@@ -15,9 +15,10 @@
 namespace ttb {
 
 static const uint_fast16_t SPIN_CYCLES_BEFORE_YIELD = 20000;
+static const uint_fast16_t SPIN_CYCLES_BEFORE_YIELD_FAIR = 500;
 
 /**
- * @brief A simple unfair spin lock that satisfies LockAble.
+ * @brief A simple unfair spin lock that satisfies Lockable.
  */
 class SpinLock {
 public:
@@ -38,7 +39,7 @@ public:
     }
 
     bool try_lock() {
-        return !l.test_and_set(std::memory_order_acquire);
+        return !l.test_and_set(std::memory_order_acq_rel);
     }
 
 private:
@@ -46,7 +47,39 @@ private:
 };
 
 /**
- * @brief A thin-layer wrapper for pthread_spin
+ * A fair spin lock using ticket lock algorithm that satisfies BasicLockable.
+ * This type of spin locks quickly become very slow when number of thread competing for the lock
+ * becomes large, for example tens. Use SpinLock if fairness is not a huge concern.
+ */
+class FairSpinLock {
+public:
+    FairSpinLock() :
+            next(0), active(0) {
+    }
+
+    void lock() {
+        unsigned int ticket = next.fetch_add(1, std::memory_order_acq_rel);
+        uint_fast16_t patience = SPIN_CYCLES_BEFORE_YIELD_FAIR;
+        while (active.load(std::memory_order_acquire) != ticket) {
+            patience--;
+            if (!patience) {
+                patience = SPIN_CYCLES_BEFORE_YIELD_FAIR;
+                std::this_thread::yield(); // can be insanely slow without yield
+            }
+        }
+    }
+
+    void unlock() {
+        active.fetch_add(1, std::memory_order_release);
+    }
+
+private:
+    std::atomic_uint next;
+    std::atomic_uint active;
+};
+
+/**
+ * @brief A thin-layer wrapper for pthread_spin, satisfies Lockable.
  */
 class PthreadSpinLockWrapper {
 public:
