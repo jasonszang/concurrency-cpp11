25e99f316dae75287e1b97d5f5e8b319b8689cb3
Jason S Zang <jasonszang@126.com>
2016-12-30 16:45:37 +0800

    A little cleaning of the test code

--- test/test_semaphore.h
+++ test/test_semaphore.h
@@ -5,11 +5,60 @@
 #ifndef TEST_SEMAPHORE_H_
 #define TEST_SEMAPHORE_H_
 
+#include <chrono>
+#include <cstdio>
+#include <memory>
+#include <thread>
+#include <vector>
+
+#include "../concurrency/semaphore.h"
+
 namespace ttb {
 
 namespace test {
 
-void test_semaphore();
+using std::vector;
+using std::thread;
+using std::unique_ptr;
+
+static const int TICKS = 5;
+static const int THREAD_NUMBER = 100;
+
+class ThreadFunctor {
+public:
+    ThreadFunctor(int id, Semaphore* sem) :
+            id(id), ctr(TICKS), sem(sem) {
+    }
+    void operator()() {
+        while (ctr>0) {
+            sem->acquire(id);
+            printf("thread id:%d, tick counter %d, remaining sem %d\n",
+                   id, ctr--, sem->available_permits());
+            if (sem->available_permits()<0) {
+                printf("ERROR! MINUS SEMAPHORES!!\n");
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            sem->release(id);
+        }
+    }
+private:
+    int id;
+    int ctr;
+    Semaphore* sem;
+};
+
+void test_semaphore() {
+    Semaphore* sem = new Semaphore(THREAD_NUMBER);
+    vector<unique_ptr<thread> > threads;
+    for(int i=0; i<THREAD_NUMBER; ++i) {
+        ThreadFunctor func(i+1, sem);
+        thread* curThread = new thread(func);
+        threads.push_back(unique_ptr<thread>(curThread));
+    }
+    for (int i=0; i<THREAD_NUMBER; ++i) {
+        threads[i]->join();
+    }
+}
 
 } // namespace test
 
---
+++ test/test_spin_lock.h
@@ -0,0 +1,58 @@
+/*
+ * test_spin_lock.h
+ *
+ *  Created on: 2016-12-30
+ *      Author: jasonszang
+ */
+
+#ifndef TEST_TEST_SPIN_LOCK_H_
+#define TEST_TEST_SPIN_LOCK_H_
+
+#include <cstdio>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+#include "../concurrency/spin_lock.h"
+#include "../pthread_wrapper/pthread_spinlock.h"
+
+namespace ttb {
+
+namespace test {
+
+static int g = 1000000;
+ttb::SpinLock sl;
+ttb::PthreadSpinLockWrapper psw;
+ttb::FairSpinLock fsl;
+std::mutex m;
+
+inline void tf(int num) {
+    for (int i = 0; i < num; ++i) {
+//        std::lock_guard<ttb::SpinLock> l(sl);
+//        std::lock_guard<ttb::FairSpinLock> l(fsl);
+        std::lock_guard<ttb::PthreadSpinLockWrapper> l(psw);
+//        std::lock_guard<std::mutex> l(m);
+        g -= 1;
+        volatile int j = 500;
+        for (; j != 0; --j)
+            ; // do some work
+        printf("%d\n", g);
+    }
+}
+
+inline void test_spin_lock() {
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 10; ++i) {
+        threads.emplace_back(&tf, 100000);
+    }
+    for (auto &thread : threads) {
+        thread.join();
+    }
+    printf("%d\n", g);
+}
+
+} // namespace test
+
+} // namespace ttb
+
+#endif /* TEST_TEST_SPIN_LOCK_H_ */
